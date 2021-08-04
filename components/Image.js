9238fc246313ebe0d2a669d6f5830df8a1504d1d
Wayan Tisna <wayantisna74@gmail.com>
2021-08-04 09:52:13 +0800

    update netlify

--- components/Image.js
+++ components/Image.js
@@ -1,4 +1,4 @@
-import NextImage from 'next/image'
+// import NextImage from 'next/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text,@next/next/no-img-element
 const Image = ({ ...rest }) => <img {...rest} alt="" />
